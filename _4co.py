e2775c967e42126db9153bc80d33dc8934525aca
hosam ksbaa <Hosamksbaa@gmail.com>
2023-04-22 23:04:43 +0200

    yesssss

---
+++ _4co.py
@@ -0,0 +1,36 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+
+from kafka import KafkaConsumer
+import googletrans
+from googletrans import Translator
+
+from producer import send_message
+from consumer import receive_messages
+
+
+
+
+# Set up Kafka consumer
+consumer = KafkaConsumer(
+    'translated',
+    bootstrap_servers=['localhost:9092'],
+    # group_id='my-group'
+)
+
+
+
+# Receive messages from the Kafka broker until there are no more messages available.
+while True:
+    new_messages = consumer.poll(timeout_ms=1000)
+    if new_messages:
+        for _, messages in new_messages.items():
+            for message in messages:
+                text = message.value
+                text = text.decode('utf-8')
+                analyzer = SentimentIntensityAnalyzer()
+
+                # Analyze text
+                scores = analyzer.polarity_scores(text)
+                print(scores)
